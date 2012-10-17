fa9ee6b134d4ead07b5b05112f1c1a95a1376c55
viewizard <viewizard@ff192a37-909f-40e5-93bf-5d04ad84bf5f>
2012-10-17 11:29:14 +0000

    Collision detection code revised for more speed.

--- AstroMenaceSource/Object3D/Explosion/SpaceExplosion/SpaceExplosion.cpp
+++ AstroMenaceSource/Object3D/Explosion/SpaceExplosion/SpaceExplosion.cpp
@@ -483,19 +483,16 @@ void CSpaceExplosion::Create(CObject3D *Object, int ExplType, VECTOR3D ExplLocat
 			// находим точку локального положения объекта в моделе
 			VECTOR3D LocalLocation = Object->DrawObjectList[i].Location;
 			Matrix33CalcPoint(&LocalLocation, Object->CurrentRotationMat);
-			LocalLocation += Object->HitBBLocation[i];
-
-			VECTOR3D LocalLocationTmp = Object->HitBBLocation[i];
-			Matrix33CalcPoint(&LocalLocationTmp, InvRotationMat);
+			LocalLocation = Object->HitBBLocation[i]-LocalLocation;
+			Matrix33CalcPoint(&LocalLocation, InvRotationMat);
 			// и меняем внутрее положение
-			ShipPart->DrawObjectList[0].Location = LocalLocationTmp^(-1.0f);
-
+			ShipPart->DrawObjectList[0].Location = LocalLocation^(-1.0f);
 
 			// находим все данные по геометрии
 			ShipPart->InitByDrawObjectList();
 
 			// установка текущего положения и поворота
-			ShipPart->SetLocation(Object->Location+LocalLocation);
+			ShipPart->SetLocation(Object->Location+Object->HitBBLocation[i]);
 			ShipPart->SetRotation(Object->Rotation);
 
 
