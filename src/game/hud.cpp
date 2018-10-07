04ab95efe2ed9248091cbd9ad88f074f2e09f8e6
Mikhail Kurinnoi <viewizard@viewizard.com>
2018-10-07 19:57:56 +0300

    Minor improvements in InitHUDParticleSystems().

--- src/game/hud.cpp
+++ src/game/hud.cpp
@@ -55,26 +55,25 @@ float GetShipMaxEnergy(int Num);
 
 /*
  * Init head-up display particle systems.
+ * Make sure we re-init all particle systems data all the time in order to avoid
+ * issues with future code changes.
  */
 void InitHUDParticleSystems()
 {
-	EnergyParticleSystem2D = vw_CreateParticleSystem2D(vw_GetTimeThread(0));
+	if (EnergyParticleSystem2D.expired())
+		EnergyParticleSystem2D = vw_CreateParticleSystem2D(vw_GetTimeThread(0));
 	if (auto sharedEnergyParticleSystem2D = EnergyParticleSystem2D.lock()) {
-		sharedEnergyParticleSystem2D->ColorStart.r = 0.70f;
-		sharedEnergyParticleSystem2D->ColorStart.g = 0.80f;
-		sharedEnergyParticleSystem2D->ColorStart.b = 1.00f;
-		sharedEnergyParticleSystem2D->ColorEnd.r = 0.00f;
-		sharedEnergyParticleSystem2D->ColorEnd.g = 0.00f;
-		sharedEnergyParticleSystem2D->ColorEnd.b = 1.00f;
-		sharedEnergyParticleSystem2D->AlphaStart = 1.00f;
-		sharedEnergyParticleSystem2D->AlphaEnd = 1.00f;
-		sharedEnergyParticleSystem2D->SizeStart = 12.00f;
-		sharedEnergyParticleSystem2D->SizeVar = 10.00f;
-		sharedEnergyParticleSystem2D->SizeEnd = 0.00f;
-		sharedEnergyParticleSystem2D->Speed = 70.00f;
-		sharedEnergyParticleSystem2D->SpeedVar = 20.00f;
-		sharedEnergyParticleSystem2D->Theta = 360.00f;
-		sharedEnergyParticleSystem2D->Life = 2.10f;
+		sharedEnergyParticleSystem2D->ColorStart = sRGBCOLOR{0.7f, 0.8f, 1.0f};
+		sharedEnergyParticleSystem2D->ColorEnd = sRGBCOLOR{0.0f, 0.0f, 1.0f};
+		sharedEnergyParticleSystem2D->AlphaStart = 1.0f;
+		sharedEnergyParticleSystem2D->AlphaEnd = 1.0f;
+		sharedEnergyParticleSystem2D->SizeStart = 12.0f;
+		sharedEnergyParticleSystem2D->SizeVar = 10.0f;
+		sharedEnergyParticleSystem2D->SizeEnd = 0.0f;
+		sharedEnergyParticleSystem2D->Speed = 70.0f;
+		sharedEnergyParticleSystem2D->SpeedVar = 20.0f;
+		sharedEnergyParticleSystem2D->Theta = 360.0f;
+		sharedEnergyParticleSystem2D->Life = 2.1f;
 		sharedEnergyParticleSystem2D->LifeVar = 0.05f;
 		sharedEnergyParticleSystem2D->ParticlesPerSec = 50;
 		sharedEnergyParticleSystem2D->IsMagnet = true;
@@ -84,81 +83,72 @@ void InitHUDParticleSystems()
 		sharedEnergyParticleSystem2D->MoveSystem(sVECTOR3D{33.0f, 29.0f, 0.0f});
 	}
 
-	Life3ParticleSystem2D = vw_CreateParticleSystem2D(vw_GetTimeThread(0));
+	if (Life3ParticleSystem2D.expired())
+		Life3ParticleSystem2D = vw_CreateParticleSystem2D(vw_GetTimeThread(0));
 	if (auto sharedLife3ParticleSystem2D = Life3ParticleSystem2D.lock()) {
-		sharedLife3ParticleSystem2D->ColorStart.r = 1.00f;
-		sharedLife3ParticleSystem2D->ColorStart.g = 0.40f;
-		sharedLife3ParticleSystem2D->ColorStart.b = 0.10f;
-		sharedLife3ParticleSystem2D->ColorEnd.r = 0.50f;
-		sharedLife3ParticleSystem2D->ColorEnd.g = 0.00f;
-		sharedLife3ParticleSystem2D->ColorEnd.b = 0.00f;
-		sharedLife3ParticleSystem2D->AlphaStart = 1.00f;
-		sharedLife3ParticleSystem2D->AlphaEnd = 1.00f;
-		sharedLife3ParticleSystem2D->SizeStart = 13.00f;
-		sharedLife3ParticleSystem2D->SizeVar = 5.00f;
-		sharedLife3ParticleSystem2D->SizeEnd = 0.00f;
-		sharedLife3ParticleSystem2D->Speed = 0.00f;
-		sharedLife3ParticleSystem2D->SpeedOnCreation = 8.00f;
-		sharedLife3ParticleSystem2D->SpeedVar = 0.00f;
-		sharedLife3ParticleSystem2D->Theta = 360.00f;
-		sharedLife3ParticleSystem2D->Life = 2.00f;
+		sharedLife3ParticleSystem2D->ColorStart = sRGBCOLOR{1.0f, 0.4f, 0.1f};
+		sharedLife3ParticleSystem2D->ColorEnd = sRGBCOLOR{0.5f, 0.0f, 0.0f};
+		sharedLife3ParticleSystem2D->AlphaStart = 1.0f;
+		sharedLife3ParticleSystem2D->AlphaEnd = 1.0f;
+		sharedLife3ParticleSystem2D->SizeStart = 13.0f;
+		sharedLife3ParticleSystem2D->SizeVar = 5.0f;
+		sharedLife3ParticleSystem2D->SizeEnd = 0.0f;
+		sharedLife3ParticleSystem2D->Speed = 0.0f;
+		sharedLife3ParticleSystem2D->SpeedOnCreation = 8.0f;
+		sharedLife3ParticleSystem2D->SpeedVar = 0.0f;
+		sharedLife3ParticleSystem2D->Theta = 360.0f;
+		sharedLife3ParticleSystem2D->Life = 2.0f;
 		sharedLife3ParticleSystem2D->LifeVar = 0.05f;
 		sharedLife3ParticleSystem2D->ParticlesPerSec = 50;
 		sharedLife3ParticleSystem2D->CreationType = eParticle2DCreationType::Quad;
-		sharedLife3ParticleSystem2D->CreationSize = sVECTOR3D{1.0f, 18.0f, 0.0f};
+		sharedLife3ParticleSystem2D->CreationSize(1.0f, 18.0f, 0.0f);
 		sharedLife3ParticleSystem2D->Texture = GetPreloadedTextureAsset("gfx/flare1.tga");
 		sharedLife3ParticleSystem2D->MoveSystem(sVECTOR3D{GameConfig().InternalWidth - 33.0f, 29.0f, 0.0f});
 	}
 
-	Life2ParticleSystem2D = vw_CreateParticleSystem2D(vw_GetTimeThread(0));
+	if (Life2ParticleSystem2D.expired())
+		Life2ParticleSystem2D = vw_CreateParticleSystem2D(vw_GetTimeThread(0));
 	if (auto sharedLife2ParticleSystem2D = Life2ParticleSystem2D.lock()) {
-		sharedLife2ParticleSystem2D->ColorStart.r = 1.00f;
-		sharedLife2ParticleSystem2D->ColorStart.g = 0.40f;
-		sharedLife2ParticleSystem2D->ColorStart.b = 0.10f;
-		sharedLife2ParticleSystem2D->ColorEnd.r = 0.50f;
-		sharedLife2ParticleSystem2D->ColorEnd.g = 0.00f;
-		sharedLife2ParticleSystem2D->ColorEnd.b = 0.00f;
-		sharedLife2ParticleSystem2D->AlphaStart = 1.00f;
-		sharedLife2ParticleSystem2D->AlphaEnd = 1.00f;
-		sharedLife2ParticleSystem2D->SizeStart = 13.00f;
-		sharedLife2ParticleSystem2D->SizeVar = 5.00f;
-		sharedLife2ParticleSystem2D->SizeEnd = 0.00f;
-		sharedLife2ParticleSystem2D->Speed = 0.00f;
-		sharedLife2ParticleSystem2D->SpeedOnCreation = 8.00f;
-		sharedLife2ParticleSystem2D->SpeedVar = 0.00f;
-		sharedLife2ParticleSystem2D->Theta = 360.00f;
-		sharedLife2ParticleSystem2D->Life = 2.00f;
+		sharedLife2ParticleSystem2D->ColorStart = sRGBCOLOR{1.0f, 0.4f, 0.1f};
+		sharedLife2ParticleSystem2D->ColorEnd = sRGBCOLOR{0.5f, 0.0f, 0.0f};
+		sharedLife2ParticleSystem2D->AlphaStart = 1.0f;
+		sharedLife2ParticleSystem2D->AlphaEnd = 1.0f;
+		sharedLife2ParticleSystem2D->SizeStart = 13.0f;
+		sharedLife2ParticleSystem2D->SizeVar = 5.0f;
+		sharedLife2ParticleSystem2D->SizeEnd = 0.0f;
+		sharedLife2ParticleSystem2D->Speed = 0.0f;
+		sharedLife2ParticleSystem2D->SpeedOnCreation = 8.0f;
+		sharedLife2ParticleSystem2D->SpeedVar = 0.0f;
+		sharedLife2ParticleSystem2D->Theta = 360.0f;
+		sharedLife2ParticleSystem2D->Life = 2.0f;
 		sharedLife2ParticleSystem2D->LifeVar = 0.05f;
 		sharedLife2ParticleSystem2D->ParticlesPerSec = 50;
 		sharedLife2ParticleSystem2D->CreationType = eParticle2DCreationType::Quad;
-		sharedLife2ParticleSystem2D->CreationSize = sVECTOR3D{18.0f, 1.0f, 0.0f};
+		sharedLife2ParticleSystem2D->CreationSize(18.0f, 1.0f, 0.0f);
 		sharedLife2ParticleSystem2D->Texture = GetPreloadedTextureAsset("gfx/flare1.tga");
 		sharedLife2ParticleSystem2D->MoveSystem(sVECTOR3D{GameConfig().InternalWidth - 33.0f, 29.0f, 0.0f});
 	}
 
-	LifeParticleSystem2D = vw_CreateParticleSystem2D(vw_GetTimeThread(0));
+	if (LifeParticleSystem2D.expired())
+		LifeParticleSystem2D = vw_CreateParticleSystem2D(vw_GetTimeThread(0));
 	if (auto sharedLifeParticleSystem2D = LifeParticleSystem2D.lock()) {
-		sharedLifeParticleSystem2D->ColorStart.r = 1.00f;
-		sharedLifeParticleSystem2D->ColorStart.g = 0.60f;
-		sharedLifeParticleSystem2D->ColorStart.b = 0.20f;
-		sharedLifeParticleSystem2D->ColorEnd.r = 0.50f;
-		sharedLifeParticleSystem2D->ColorEnd.g = 0.00f;
-		sharedLifeParticleSystem2D->ColorEnd.b = 0.00f;
-		sharedLifeParticleSystem2D->AlphaStart = 1.00f;
-		sharedLifeParticleSystem2D->AlphaEnd = 1.00f;
-		sharedLifeParticleSystem2D->SizeStart = 25.00f;
-		sharedLifeParticleSystem2D->SizeVar = 5.00f;
-		sharedLifeParticleSystem2D->SizeEnd = 0.00f;
-		sharedLifeParticleSystem2D->Speed = 0.00f;
-		sharedLifeParticleSystem2D->SpeedOnCreation = 8.00f;
-		sharedLifeParticleSystem2D->SpeedVar = 10.00f;
-		sharedLifeParticleSystem2D->Theta = 360.00f;
-		sharedLifeParticleSystem2D->Life = 1.50f;
+		sharedLifeParticleSystem2D->ColorStart = sRGBCOLOR{1.0f, 0.6f, 0.2f};
+		sharedLifeParticleSystem2D->ColorEnd = sRGBCOLOR{0.5f, 0.0f, 0.0f};
+		sharedLifeParticleSystem2D->AlphaStart = 1.0f;
+		sharedLifeParticleSystem2D->AlphaEnd = 1.0f;
+		sharedLifeParticleSystem2D->SizeStart = 25.0f;
+		sharedLifeParticleSystem2D->SizeVar = 5.0f;
+		sharedLifeParticleSystem2D->SizeEnd = 0.0f;
+		sharedLifeParticleSystem2D->Speed = 0.0f;
+		sharedLifeParticleSystem2D->SpeedOnCreation = 8.0f;
+		sharedLifeParticleSystem2D->SpeedVar = 10.0f;
+		sharedLifeParticleSystem2D->Theta = 360.0f;
+		sharedLifeParticleSystem2D->Life = 1.5f;
 		sharedLifeParticleSystem2D->LifeVar = 0.05f;
 		sharedLifeParticleSystem2D->ParticlesPerSec = 70;
-		sharedLifeParticleSystem2D->Direction = sVECTOR3D{1.0f, 0.0f, 0.0f};
+		sharedLifeParticleSystem2D->Direction(1.0f, 0.0f, 0.0f);
 		sharedLifeParticleSystem2D->CreationType = eParticle2DCreationType::Circle;
-		sharedLifeParticleSystem2D->CreationSize = sVECTOR3D{25.0f, 25.0f, 0.0f};
+		sharedLifeParticleSystem2D->CreationSize(25.0f, 25.0f, 0.0f);
 		sharedLifeParticleSystem2D->DeadZone = 24.0f;
 		sharedLifeParticleSystem2D->IsMagnet = true;
 		sharedLifeParticleSystem2D->MagnetFactor = 25.0f;
